5b8e089e3c7e3f5ddac16689a341d086404c107a
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-20 14:31:16 +0100

    Make the lvl 9

---
+++ exercicesIntro/L9/components/productDetails.js
@@ -0,0 +1,11 @@
+app.component('product-details', {
+    props: {
+        details: {
+            type: Array
+
+        }
+    },
+    template: 
+    /*html*/
+    `<p>tqt pas pour les détails</p>`
+})
